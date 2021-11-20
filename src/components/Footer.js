b4aa85571b7b8f08bb6a3912b4d05720e6334722
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-19 23:30:20 -0600

    adding a button in the footer for go to the admin panel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Footer = () => {
   return (
@@ -6,8 +7,14 @@ const Footer = () => {
       <div>
         <h4>
           copyright&copy;{new Date().getFullYear()} <br></br>
-          PAPRI all rights reserved Footer
+          PAPRI all rights reserved
         </h4>
+        <a
+          href="https://portfolio-admin-papri.herokuapp.com/admin/"
+          className="btn"
+        >
+          Manage the API from here (ADMIN PANEL)
+        </a>
       </div>
     </footer>
   )

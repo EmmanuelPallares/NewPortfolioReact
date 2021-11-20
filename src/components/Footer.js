8d773cb488ebe38cf33749a902335f5555e10394
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-19 22:45:19 -0600

    test for vercel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react"
 
 const Footer = () => {
-  return <footer className="footer">
-    <div>
-     
-      <h4>
+  return (
+    <footer className="footer">
+      <div>
+        <h4>
           copyright&copy;{new Date().getFullYear()} <br></br>
-          PAPRI all rights reserved
+          PAPRI all rights reserved Footer
         </h4>
-    </div>
-  </footer>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer

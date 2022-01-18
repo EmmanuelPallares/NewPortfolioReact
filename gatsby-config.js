a60f0b85584a989b01e8659418be762f5d2d9146
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-01-18 11:42:14 -0600

    fetch data from strapi succesfully, apiUrl change https for http

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `https://localhost:1337`,
+        apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         //contentTypes : `jobs`, `projects`, `blogs`,
         //singleType : `about`

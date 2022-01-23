b767cb623a7b9f04eb14a47515a181005caa4133
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-01-22 20:39:25 -0600

    I comments the images from fluid childimagesharp

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Image from "gatsby-image"
+// import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = ({ description, title, github, stack, url, image, index }) => {
+const Project = ({ description, title, github, stack, url, index }) => {
   return (
     <article className="project">
-      {image && (
+      {/* {image && (
         <Image fluid={image.childImageSharp.fluid} className="project-img" />
-      )}
+      )} */}
 
       <div className="project-info">
         <span className="project-number"> 0{index + 1}</span>
